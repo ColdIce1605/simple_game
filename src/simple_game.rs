abbd53f74e270b4edfc895e8a1f5337da1e5dc07
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-12 09:51:45 -0600

    Update simple_game.rs

--- src/simple_game.rs
+++ src/simple_game.rs
@@ -53,6 +53,13 @@ use rand::Rng;
 	      }
         }  
 	      println!("Random Integers: {}", rng1); 
+		if rng1 = 0 {
+		hp = 0;
+		println!("HP: {}", hp);
+		}
+		if hp = 0 {
+		println!("You've died");
+		}
 	   
 	      //do not place anything after this
 }
